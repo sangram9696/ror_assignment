2fa1dda12932a0d2db26f32299547a77d11a10f6
Sangram <sangram.desai@wgblindia.com>
2017-02-23 11:46:46 +0530

    d

--- assignment3/customer.rb
+++ assignment3/customer.rb
@@ -1,10 +1,8 @@
 class Customer
 	def show_customer_options
-		puts "1.Add Product"
-		puts "2.Remove Product"
-		puts "3.List Product"
-		puts "4.Edit Product "
-		puts "5.Search Product"
+		puts "1.List Product"
+		puts "2.Search Product"
+		puts "3.Buy Product"		
 		puts "Select oprion:- "
 		self.get_customer_options
 	end
@@ -12,18 +10,42 @@ class Customer
 		option=gets.chomp.to_i
 		case option
 		when 1   
-		  puts "it was 1" 
+		  self.list_prodcut
 		when 2    
-			puts "it was 2" 
+		 self.search_prodcut
 		when 3   
-		  puts "it was 3" 
-		when 4    
-			puts "it was 4" 
-		when 5    
-			puts "it was 5" 						
+		 self.buy_prodcut			
 		else
 		  puts "it was something else"
 		end		
 	end
 
+	def buy_prodcut
+		self.list_product
+		pro= Product.new
+		puts "Enter product id to buy product:- "
+		@product_id=STDIN.gets.chomp
+		
+		puts "Enter your name:- "
+		@user_name=STDIN.gets.chomp		
+		
+		puts "Enter your card number:- "
+		@card_number=STDIN.gets.chomp	
+
+		puts "Enter your card cvv:- "
+		@cvv=STDIN.gets.chomp	
+
+		pro.buy(@product_id,@user_name,@card_number,@cvv)	
+		self.list_product
+	end	
+	def search_prodcut
+		puts "Search product:- "
+		@word=STDIN.gets.chomp		
+		pro= Product.new
+		pro.search(@word)
+	end	
+	def list_product
+		pro= Product.new
+		pro.list
+	end
 end
--- assignment3/product.rb
+++ assignment3/product.rb
@@ -17,7 +17,8 @@ class Product
 
 		File.open("inventory.txt", "r"){|file| 
 			file.each_line do |line|
-					if line[0]==product_id.to_s
+				result = line.split("|").map(&:strip)
+					if result[0]==product_id.to_s
 				File.open("inventory_temp.txt", "a+"){|filetemp| 
 					line_no=(count).to_s	
 					filetemp.puts line_no+"|"+data
@@ -48,7 +49,8 @@ class Product
 
 		File.open("inventory.txt", "r"){|file| 
 			file.each_line do |line|
-					if line[0]!=product_id.to_s
+					result = line.split("|").map(&:strip)
+					if result[0]!=product_id.to_s
 						File.open("inventory_temp.txt", "a+"){|filetemp| 
 							line_no=(count).to_s	
 							filetemp.puts line
@@ -82,6 +84,52 @@ class Product
 		     end	
 		  end
 		end					
+	end	
+
+	def add_user_order_info(product_id,user_name,card_number,cvv)
+		data=product_id.to_s+"|"+user_name+"|"+card_number.to_s+"|"+cvv.to_s
+		File.open("orders.txt", "a+"){|file| 
+			line_no=(file.count+1).to_s	
+			file.puts line_no+"|"+data
+		}
+	end	
+
+	def buy(product_id,user_name,card_number,cvv)
+		count=1;
+		File.open("inventory_temp.txt", "wb") do |input|
+		end	
+		File.open("inventory.txt", "r"){|file| 
+			file.each_line do |line|
+				result = line.split("|").map(&:strip)
+				if result[0]==product_id
+					if result[3]=="0"
+						puts "Product not avliable"
+					else
+						File.open("inventory_temp.txt", "a+"){|filetemp| 
+							line_no=(count).to_s
+							stock=((result[3].to_i)-1).to_s
+							data=result[0]+"|"+result[1]+"|"+result[2]+"|"+stock+"|"+result[4]	
+							filetemp.puts(data)
+						}	
+					 end   
+					else
+				File.open("inventory_temp.txt", "a+"){|filetemp| 
+					line_no=(count).to_s	
+					filetemp.puts line
+				}		
+					end	
+				count=count+1	
+			end
+		}
+			File.open("inventory_temp.txt", "rb") do |input|
+				File.open("inventory.txt", "wb") do |output|
+					while buff = input.read(4096)
+					output.write(buff)
+					end
+				end
+			end	
+
+			self.add_user_order_info(product_id,user_name,card_number,cvv)
 	end	
 
 end
